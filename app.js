866883ed8e7dd7d7754725f65e0fc90d211627a9
William Wilson <will.wilson468@gmail.com>
2022-08-26 10:55:08 -0400

    allow decimal index

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ function logic(score, uindex, crate, sloper, par1, pcc1) {
     return;
   }
 
-  if (0 <= userIndex && userIndex <= 5) {
+  if (0 <= userIndex && userIndex < 5.5) {
     odds = oneArray[roundDiff * -1];
       if (odds > 258) {
         document.getElementById("sbn").innerHTML = `Sandbagger. The odds of shooting this score are: ${odds} to 1.`;
@@ -47,7 +47,7 @@ function logic(score, uindex, crate, sloper, par1, pcc1) {
         // console.log(`The players net differential is ${roundDiff}`);
       }
   } else {
-    if (6 <= userIndex && userIndex <= 12) {
+    if (5.5 <= userIndex && userIndex < 12.5) {
       odds = twoArray[roundDiff * -1];
 
         if (odds > 258) {
@@ -59,7 +59,7 @@ function logic(score, uindex, crate, sloper, par1, pcc1) {
         }
       
     } else {
-      if (13 <= userIndex && userIndex <= 21) {
+      if (12.5 <= userIndex && userIndex < 21.5) {
         odds = threeArray[roundDiff * -1];
           if (odds > 258) {
             document.getElementById("sbn").innerHTML = `Sandbagger. The odds of shooting this score are: ${odds} to 1.`;
@@ -71,7 +71,7 @@ function logic(score, uindex, crate, sloper, par1, pcc1) {
 
        
       } else {
-        if (22 <= userIndex && userIndex <= 30) {
+        if (21.5 <= userIndex && userIndex < 30) {
           odds = fourArray[roundDiff * -1];
             if (odds > 258) {
               document.getElementById("sbn").innerHTML = `Sandbagger. The odds of shooting this score are: ${odds} to 1.`;
@@ -82,7 +82,7 @@ function logic(score, uindex, crate, sloper, par1, pcc1) {
             }
           
         } else {
-          if (userIndex > 30) {
+          if (userIndex => 30) {
             odds = fiveArray[roundDiff * -1];
               if (odds > 258) {
                 document.getElementById("sbn").innerHTML = `Sandbagger. The odds of shooting this score are: ${odds} to 1.`;
